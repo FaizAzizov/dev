4a7a9a206ae4881c802e0df3179bda3ae7710764
Arleen <iluziya-obmana@mail.ru>
2023-11-03 01:25:22 +0500

    rewrite js file

--- script.js
+++ script.js
@@ -10,5 +10,41 @@ const foo = 'qwe'
 console.log(variable + name2);
 
 
-let b;
-console.log(b);
+let c;
+console.log(c);
+
+const a = true
+const b = false
+
+let isOpened = 1 < 2 //True
+
+if (a || b) {
+    console.log('Ýes');
+}
+
+const str = 'string "into"'
+const str2 = "string 'into'"
+const str3 = `srting ${isOpened} value`
+
+const person = {
+    name: 'Alex',
+    age: 25,
+    group: 'A45'
+}
+
+const person2 = {
+    name: 'Sam',
+    age: 25,
+    group: 'A45',
+    children: [
+        {
+            name: 'Sam',
+            age: 10,
+            group: null,
+        }
+    ]
+}
+
+const arr = [1,2,3,4]
+const arr2 = ['1','2','3','4']
+const arr3 = [1,'2','3', {name:'asdas'}]
